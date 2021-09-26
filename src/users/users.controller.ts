1b24e7df13d1655154e8314a952d96c28ccc1b20
Abu Saleh  Faysal <asfaysal.bracu@gmail.com>
2021-09-27 01:07:20 +0600

    swagger integraded

---
+++ src/users/users.controller.ts
@@ -0,0 +1,30 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+import { User } from './entities/user.entity';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+
+@ApiTags('users')
+@Controller('users')
+export class UsersController {
+
+    constructor(private usersService: UsersService) {}
+    
+    @ApiOkResponse({type: User, isArray: true})
+    @Get()
+    getUsers(): User[] {
+        return this.usersService.findAll();
+    }
+
+    @ApiOkResponse({type: User})
+    @Get(':id')
+    getUserById(@Param('id') id: string): User {
+        return this.usersService.findById(Number(id));
+    }
+
+    @ApiCreatedResponse({type: User})
+    @Post()
+    createUser(@Body() body: CreateUserDto):User {
+        return this.usersService.createUser(body);
+    }
+}
